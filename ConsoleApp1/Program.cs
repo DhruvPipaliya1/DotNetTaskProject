a6cf370ebb196265181ceacb0137bf9fcacaf673
Dhruv <dpipaliya159@gmail.com>
2026-01-06 16:46:23 +0530

    problem9Completed

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -26,9 +26,52 @@ namespace project
             //Console.WriteLine("Updated hour: " + AttendenceTracker.UpdateTotalHour(todaysHour, attendence));
 
 
-            int[] parkHour = {2, 5, 1, 8, 3};
-            var ParkCalculator = new ParkingFeeCalculator();
-            Console.WriteLine("Total Fee: " + ParkCalculator.CalculateFullDaysParkers(parkHour));
+
+            //int[] parkHour = {2, 5, 1, 8, 3};
+            //var ParkCalculator = new ParkingFeeCalculator();
+            //Console.WriteLine("Total Fee: " + ParkCalculator.TotalParkingFee(parkHour));
+            //Console.WriteLine("Full Day Parkers: " + ParkCalculator.CalculateFullDaysParkers(parkHour));
+            //Console.Write("Enter Parking Hours: ");
+            //int userInput = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Your Fee: " + ParkCalculator.UserParkingFee(userInput));
+
+
+
+            //int[] stocks = { 50, 5, 25, 0, 15 };
+            //var CalculateStock = new InventoryStockCheck();
+            //Console.WriteLine("Out of Stock: " + CalculateStock.CalculateOutOfStockItems(stocks));
+            //Console.WriteLine("Low Stock items: " + CalculateStock.CalculateLowStockItems(stocks));
+            //Console.Write("Enter item number (0-4): ");
+            //int stockNumber = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter restock quantity: ");
+            //int stockQuantity = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("New Quantity: " + CalculateStock.AddNewItem(stockNumber, stockQuantity, stocks));
+
+
+
+            //int[] units = { 150, 200, 180, 220, 160 };
+            //var ElectricityBill = new ElectricityBillCalculator();
+            //Console.WriteLine("Total Units: " + ElectricityBill.TotalElectricityUnits(units));
+            //Console.WriteLine("High Usage Months: " + ElectricityBill.HighUsageMonthsCount(units));
+            //Console.Write("Enter Current Usage: ");
+            //int userInput = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Your Bill: " + ElectricityBill.CurrentUsage(userInput, units));
+
+
+
+
+            //int[] number = { 4, 5, 6 };
+            //var CalculateFactorial = new NumberFactorialCalculator();
+            //for(int i=0; i<number.Length; i++)
+            //{
+            //    Console.WriteLine($"{number[i]}! = {CalculateFactorial.FindFactorial(number[i])}");
+            //}
+            //Console.Write("Enter a Number: ");
+            //var userInput = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"{userInput}! = {CalculateFactorial.FindFactorial(userInput)}");
+
+
+
         }
     }
 
